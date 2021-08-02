041311f78c5d64ad73230fb959a10aef6a7da098
Muhammad Fahad Baig <mfahad@techtix.co>
2021-08-02 19:40:57 +0500

    Created  receipt view controller

---
+++ LoadX Transporter/PK Modules/Earning/UploadReceiptViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UploadReceptViewController.swift
+//  LoadX Transporter
+//
+//  Created by Mehsam Saeed on 02/08/2021.
+//  Copyright © 2021 BIAD Services Ltd. All rights reserved.
+//
+
+import UIKit
+
+class UploadReceiptViewController: UIViewController {
+    @IBOutlet weak var shadowView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+// Do any additional setup after loading the view.
+    }
+
+}
